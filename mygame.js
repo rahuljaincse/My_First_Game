3091d27242c4c317fb01fd4d9667a09c2b425ada
Rahul Jain <rahuljaincse@users.noreply.github.com>
2014-05-14 12:33:11 +0530

    Update mygame.js

--- mygame.js
+++ mygame.js
@@ -8,45 +8,50 @@
 
 // Check if the user is ready to play!
 
-  confirm ("I am ready to play!");
-var age=prompt("What's your age?");
+    confirm ("I am ready to play!");
+    
+    var age=prompt("What's your age?");
 
   
-    if(age<13){
+if(age<13)
+{
     
     console.log ("You are allowed to play but we could not take responsibility.");
 }
-else{
+else
+{
     console.log("Play We Are With You  Guys!!! :-) :-)");
-    } 
+} 
     
     console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
     
-console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+    console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
 
 
-var userAnswer=prompt("Do you want to race Bieber on stage?");
+    var userAnswer=prompt("Do you want to race Bieber on stage?");
 
 
 
-    if(userAnswer==="yes")
-    {
+if(userAnswer==="yes")
+{
         
-        console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
-        }
-        else
-        {
-            console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+}
+else
+{
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
             
-            }
-            var feedback=prompt( "Please FeedBack Us For  Your Better Approach!! out of 10 ");
-  if(feedback>8){
-      console.log("Thank you! We should race at the next concert!");
+}
+    var feedback=prompt( "Please FeedBack Us For  Your Better Approach!! out of 10 ");
+  
+    if(feedback>8)
+{
+    console.log("Thank you! We should race at the next concert!");
       
-      }
-      else{
-          console.log("I;ll keep practicing coding and racing. ");
+}
+    else
+{
+console.log("I;ll keep practicing coding and racing. ");
           
-          
-          }
+}
           
