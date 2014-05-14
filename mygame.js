1281816a496101fb425b961ba70a10b52284d2bc
rahuljaincse <jenumjaytishashnum>
2014-05-14 12:29:02 +0530

    update

---
+++ mygame.js
@@ -0,0 +1,52 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * @Author:- Rahul Jain
+ */
+
+
+// Check if the user is ready to play!
+
+  confirm ("I am ready to play!");
+var age=prompt("What's your age?");
+
+  
+    if(age<13){
+    
+    console.log ("You are allowed to play but we could not take responsibility.");
+}
+else{
+    console.log("Play We Are With You  Guys!!! :-) :-)");
+    } 
+    
+    console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+    
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+
+var userAnswer=prompt("Do you want to race Bieber on stage?");
+
+
+
+    if(userAnswer==="yes")
+    {
+        
+        console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+        }
+        else
+        {
+            console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+            
+            }
+            var feedback=prompt( "Please FeedBack Us For  Your Better Approach!! out of 10 ");
+  if(feedback>8){
+      console.log("Thank you! We should race at the next concert!");
+      
+      }
+      else{
+          console.log("I;ll keep practicing coding and racing. ");
+          
+          
+          }
+          
